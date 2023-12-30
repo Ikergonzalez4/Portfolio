57e75a4479be0291af25b9a6f2ec62a70929796f
ikergonzalez4 <ikergc1989@gmail.com>
2023-12-30 07:32:09 +0100

    new features on projects :D

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,20 +6,23 @@ import firefoxImage from '../../assets/images/firefox.png'; // Importa la imagen
 import discordImage from '../../assets/images/discord.jpg'; // Importa la imagen
 import teslaImage from '../../assets/images/tesla.png'; // Importa la imagen
 import { Java, Html, Css, ReactJs } from './tech/Tech'; // Fix the casing of the file name
+import safariTesla from '../../assets/images/safari-tesla.png'; 
+import safariTeslaBkg from '../../assets/images/safari-tesla-bkg.png';
+import yourhometick from '../../assets/images/yourhometick.png';
+import pokedex from '../../assets/images/pokedex.png';
 
 
 const Projects = () => {
     // Projects.jsx
 
     const projects = [
-        { id: 1, title: 'Firefox extension', description: [<Java />, <Css />, <Html />, <ReactJs />, <Css />], image: firefoxImage },
-        { id: 2, title: 'Discord Clone', description: [<Java />, <Css />], image: discordImage },
-        { id: 3, title: 'Tesla landing page', description: [<Java />, <Css />], image: teslaImage },
-        { id: 4, title: 'Your Home Tick', description: [<Java />, <Css />], image: 'image3.jpg' },
-    ];
+        { id: 1, title: 'Tesla landing page', moreInfo: 'Lorem ipsum dolor sit amet consectetur adipiscing elit vivamus leo, venenatis commodo facilisis nullam tempor quam neque suscipit inceptos curae, habitasse mauris pulvinar sed molestie facilisi non ac. Ad aenean in justo vel viverra cubilia' , description: [<Java />, <Css />], image: safariTesla },
+        { id: 2, title: 'Esland page', moreInfo: 'Lorem ipsum dolor sit amet consectetur adipiscing elit vivamus leo, venenatis commodo facilisis nullam tempor quam neque suscipit inceptos curae, habitasse mauris pulvinar sed molestie facilisi non ac. Ad aenean in justo vel viverra cubilia', description: [<Java />, <Css />], image: safariTeslaBkg },
+        { id: 3, title: 'Your Home Tick', moreInfo: 'Lorem ipsum dolor sit amet consectetur adipiscing elit vivamus leo, venenatis commodo facilisis nullam tempor quam neque suscipit inceptos curae, habitasse mauris pulvinar sed molestie facilisi non ac. Ad aenean in justo vel viverra cubilia.', description: [<Java />, <Css />], image: yourhometick },
+        { id: 4, title: 'Back End Pokedex', moreInfo: 'Lorem ipsum dolor sit amet consectetur adipiscing elit vivamus leo, venenatis commodo facilisis nullam tempor quam neque suscipit inceptos curae, habitasse mauris pulvinar sed molestie facilisi non ac. Ad aenean in justo vel viverra cubilia.', description: [<Java />, <Css />], image: pokedex },    ];
     return (
         <div className="main-container">
-            <h1 className="title-projects">Projects</h1>
+            <h1 className="title-projects underline-effect">Projects</h1>
             <div className="projects-container">
                 {projects.map((project) => (
                     <Project key={project.id} project={project} />
--- src/components/Projects/target/Project.jsx
+++ src/components/Projects/target/Project.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './Project.css';
 
 const Project = ({ project }) => {
+    const [isImageExpanded, setIsImageExpanded] = React.useState(false);
+
+    // Agrega un manejador de eventos de clic al body cuando la imagen está expandida
+    useEffect(() => {
+        if (isImageExpanded) {
+            const closeImage = () => setIsImageExpanded(false);
+            document.body.addEventListener('click', closeImage);
+
+            // Limpia el manejador de eventos cuando la imagen ya no está expandida
+            return () => document.body.removeEventListener('click', closeImage);
+        }
+    }, [isImageExpanded]);
+
     return (
         <div className="project-container">
-            <img src={project.image} alt={project.title} className="project-image" />
             <div className="project-text">
                 <h2 className="project-title">{project.title}</h2>
+                <p className="more-info">{project.moreInfo}</p> {/* Nuevo apartado more-info */}
                 <p className="project-description">{project.description}</p>
             </div>
+            <div></div> {/* Nuevo contenedor vacío */}
+            <img src={project.image} alt={project.title} className={`project-image ${isImageExpanded ? 'expanded' : ''}`} onClick={(e) => { e.stopPropagation(); setIsImageExpanded(!isImageExpanded); }} />
         </div>
     );
 };
